fd662a54d9da4c24d802ee3e05efd2281b9223c8
DonPedro228 <Roman.Kos.RI.2023@lpnu.ua>
2023-09-22 22:02:25 +0300

    first master commit

---
+++ Laba 2.1/Solution.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+    double a; 
+    double z1;
+    //double z2; 
+
+    std::cout << "Input ";
+    std::cin >> a;
+
+    z1 = cos(a) + sin(a) / (cos(a) - sin(a));
+    double tan_squared = tan(a) * tan(a);
+    //z2 = tan_squared + (1.0 / (cos(a) * cos(a)));
+
+    std::cout << "Result z1: z = " << z1 << std::endl;
+    //std::cout << "Result z2: z = " << z2 << std::endl;
+
+    return 0;
+}
